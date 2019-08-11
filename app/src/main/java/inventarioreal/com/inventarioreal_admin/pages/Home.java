9bce15e2614a6b01cb140ab85168607ac2a2784d
Javier Leonardo Leon Nuñez <javdo2@gmail.com>
2019-08-11 13:00:20 -0500

    Fix menu devoluciones

--- app/src/main/java/inventarioreal/com/inventarioreal_admin/pages/Home.java
+++ app/src/main/java/inventarioreal/com/inventarioreal_admin/pages/Home.java
@@ -7,6 +7,7 @@ import com.daimajia.androidanimations.library.Techniques;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import inventarioreal.com.inventarioreal_admin.R;
+import inventarioreal.com.inventarioreal_admin.pages.Devoluciones.HomeDevoluciones;
 import inventarioreal.com.inventarioreal_admin.pages.Inventario.Inventarios.InventarioParcialHome;
 import inventarioreal.com.inventarioreal_admin.pages.Inventario.InventariosColaborativos.InventariosColaborativosHome;
 import inventarioreal.com.inventarioreal_admin.pages.Reportes.HomeReportes;
@@ -75,6 +76,12 @@ public class Home extends CicloActivity {
             }
         });
 
+        add_on_click(R.id.btnDev, new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                admin.callIntent(HomeDevoluciones.class, null);
+            }
+        });
         add_on_click(R.id.btnRep, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
